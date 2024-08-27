9de24fb11ab9d770d4f84de163565be425cf37ae
g-battaglia <battaglia.giacomo@icloud.com>
2024-08-27 22:28:04 +0200

    Updates

--- app/middleware/secret_key_checker_middleware.py
+++ app/middleware/secret_key_checker_middleware.py
@@ -2,16 +2,19 @@
     This is part of Astrologer API (C) 2023 Giacomo Battaglia
 """
 
-from starlette.datastructures import URL, Headers
-from starlette.responses import JSONResponse, RedirectResponse, Response
+from starlette.datastructures import Headers
+from starlette.responses import JSONResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
-
+import logging
 
 class SecretKeyCheckerMiddleware:
     def __init__(self, app: ASGIApp, secret_key_name: str, secret_keys: list = []) -> None:
         self.app = app
         self.allowed_hosts = list(secret_keys)
         self.secret_key_name = secret_key_name
+
+        if self.secret_key_name or not self.allowed_hosts:
+            logging.critical("Secret key name or allowed hosts not set. The middleware will let all requests pass through!")
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         headers = Headers(scope=scope)
